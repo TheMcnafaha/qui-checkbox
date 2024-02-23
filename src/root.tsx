844bdd8f13beaf5228dcc8cd3241314b1d4b4cab
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2024-02-22 20:53:07 -0500

    tw added

--- src/root.tsx
+++ src/root.tsx
@@ -23,7 +23,7 @@ export default component$(() => {
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
       </head>
-      <body lang="en">
+      <body lang="en" class="bg-slate-800 text-white">
         <RouterOutlet />
         <ServiceWorkerRegister />
       </body>
